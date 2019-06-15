b306ee15949658362e7f18f561daeed9c445ee0d
Mohammad Mahdi Mojahed <mmojahed93@gmail.com>
2019-06-15 21:11:46 +0430

    ref: Removes useless lines

--- src/ir/ac/modares/Main.java
+++ src/ir/ac/modares/Main.java
@@ -55,26 +55,6 @@ public class Main {
         System.out.println("[testRSA] Count of failure: " + count);
     }
 
-//    // extended Euclidean Algorithm
-//    public static int gcdExtended(int a, int b, int x, int y) {
-//        // Base Case
-//        if (a == 0) {
-//            x = 0;
-//            y = 1;
-//            return b;
-//        }
-//
-//        int x1 = 1, y1 = 1; // To store results of recursive call
-//        int gcd = gcdExtended(b % a, a, x1, y1);
-//
-//        // Update x and y using results of recursive
-//        // call
-//        x = y1 - (b / a) * x1;
-//        y = x1;
-//
-//        return gcd;
-//    }
-
     public static BigInteger gcdExtended(BigInteger a, BigInteger b) {
         BigInteger nextR;
         BigInteger prevR = b;
